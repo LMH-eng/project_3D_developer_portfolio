d5a92f55a9e50953c7a05d7c091c0464d9b6621e
LMH-eng <2364232584@qq.com>
2026-03-13 10:03:54 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/project_3D_developer_portfolio/', // 必须和你的仓库名完全一致
 })
